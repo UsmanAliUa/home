af55f7aa0f9319c641853fb595d5e426b0679900
UsmanAliUa <158816149+UsmanAliUa@users.noreply.github.com>
2024-03-07 16:31:54 +0600

    multiplication

---
+++ src/java.js
@@ -0,0 +1,37 @@
+// //alert("Are you 18+")
+// var name="Abu Bskar";
+// var age=23;
+// document.write(name);
+// document.write(age);
+// var x=2;
+// var y=6;
+// document.write(x);
+// document.write(y);
+// alert("Are You a Robot????");
+// document.write("<b>Hello brother how are you <br/></b>");
+// document.write("<h1>Name:Usman Ali<br/></h1>");
+// console.log("75 is my Roll number<br/>");
+
+
+let i;
+for (i=1; i<=10;i++){
+    let x; x=i*6;
+    document.writeln("6 X"+i+"="+x)
+};
+// function gun(name){
+//     document.write(name+"X")
+// }
+
+for (i=1; i<=10;i++){
+    let x;
+     x=i*6;
+    document.writeln("7 X"+i+"="+x )
+};
+for (i=1; i<=10;i++){
+    let x;
+     x=i*6;
+    document.writeln("8 X"+i+"="+x )
+};
+
+
+document.getElementById('m6').innerHTML()
